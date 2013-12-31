8d417bd9f0c2ff77faa176bffa2ac082a4922c61
Calos Chen <calos.chen@splashtop.com>
2013-12-31 12:25:43 +0800

    Fix the error in ruby 1.8.7

--- lib/bootstrap/sass/rails/engine.rb
+++ lib/bootstrap/sass/rails/engine.rb
@@ -2,7 +2,7 @@ module Bootstrap
   module Sass
     module Rails
       class Engine < ::Rails::Engine
-        initializer :setup_bootstrap_sass_rails, group: :all do |app|
+        #initializer :setup_bootstrap_sass_rails, group: :all do |app|
           # Ten significant digits precision is required for calculations
           # to work out in Bootstrap 3.0 - to match LESS/JS calculations
           # bump the default precision to match IEEE-754 double precision
@@ -10,7 +10,7 @@ module Bootstrap
           if ::Sass::Script::Number.precision < 10
             ::Sass::Script::Number.precision = 15
           end
-        end
+        #end
       end
     end
   end
